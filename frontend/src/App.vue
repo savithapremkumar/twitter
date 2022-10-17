87c512442f1e20e86c19b9e3c6eab4b72938a514
Savitha <savithapremkumar@gmail.com>
2022-10-18 10:06:59 +1300

    created tweet view (form and table) on front end

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,76 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <form @submit.prevent="createTweet">
+      <div class="form-group row">
+        <input
+          type="text"
+          class="form-control col-3 mx-2"
+          placeholder="Name"
+          v-model="tweet.name"
+        >
+        <input
+          type="text"
+          class="form-control col-3 mx-2"
+          placeholder="Message"
+          v-model="tweet.message"
+        >
+        <button class="btn btn-success">Tweet</button>
+      </div>
+    </form>
+
+    <table class="table">
+      <thead>
+        <th>Time</th>
+        <th>Message</th>
+        <th>Name</th>
+      </thead>
+      <tbody>
+        <tr
+          v-for="tweet in tweets"
+          :key="tweet.id"
+        >
+          <td>{{ tweet.datetime }}</td>
+          <td>{{ tweet.message }}</td>
+          <td>{{ tweet.name }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      tweet: {
+        datetime: "",
+        message: "",
+        name: "",
+      },
+      tweets: [],
+    };
+  },
+
+  async created() {
+    await this.getTweets();
+  },
+  methods: {
+    async getTweets() {
+      var response = await fetch("http://localhost:8000/api/tweets/");
+      this.tweets = await response.json();
+    },
+    async createTweet() {
+      await this.getTweets();
+      await fetch("http://localhost:8000/api/tweets/", {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(this.tweet),
+      });
+      await this.getTweets();
+    },
+  },
+};
 </script>
 
 <style>
